76a002c21fd69ea4596376a25a3b0bbbb6f88b2a
SuperBoySugar <shveta23@gmail.com>
2023-03-31 11:42:43 +0100

    mid week homework

---
+++ src/Program_8_SymbolTriangle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * Display right angle triangle of @ using nested for loops
+ *  * @
+ *  * @@
+ *  * @@@
+ *  * @@@@
+ *  * @@@@@
+ */
+public class Program_8_SymbolTriangle {
+    public static void symbolTriangle(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("@");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        //scanner declaration for input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number of rows: ");
+        symbolTriangle(scanner.nextInt());
+        scanner.close();
+    }
+}
